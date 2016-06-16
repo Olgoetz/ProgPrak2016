ddf6b8673c33050e59c53c9b1a67498016a3c911
mstroh <mstroh@maxbook-air.fritz.box>
2016-06-17 00:34:57 +0200

    MonsterClass Update: Monsters now run after you (A*)

--- Hindi/src/pp2016/team19/shared/Monster.java
+++ Hindi/src/pp2016/team19/shared/Monster.java
@@ -2,6 +2,7 @@ package pp2016.team19.shared;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
@@ -82,7 +83,7 @@ public class Monster extends Character {
 	public void move(){
 		boolean nextWalk = (System.currentTimeMillis() - lastStep) >= cooldownWalk;
 		if(valid()){
-			if(nextWalk){	
+			if(nextWalk && AStarSearch()){	
 				switch(dir){
 					case 0 : moveUp(); break;
 					case 1 : moveRight(); break;
@@ -100,6 +101,116 @@ public class Monster extends Character {
 	public void changeDir(){
 		Random random = new Random();		
 		dir = random.nextInt(4);
+	}
+	
+	// A-Star-Algorithm to search for the player
+	public boolean AStarSearch (){
+		ArrayList<Node> openList = new ArrayList<Node>();
+		ArrayList<Node> closedList = new ArrayList<Node>();
+		Node monster = new Node(getXPos(),getYPos());
+		monster.calculateCosts();
+		
+		openList.add(monster);
+		boolean playerFound = false;
+		while (!openList.isEmpty() && !playerFound){
+			// Find the cheapest node in the openList
+			Node cheapest = openList.get(0);
+			for(Node node : openList){
+				if (node.getCalculatedCosts() < cheapest.getCalculatedCosts()){
+					cheapest = node;
+				}
+			}
+			openList.remove(cheapest);
+			
+			// Create the four neighbors (up,down,left,right)
+			Node[] neighbors = new Node[4];
+			int actX = cheapest.getXPos();
+			int actY = cheapest.getYPos();
+			
+			// Up-Neighbor valid?
+			if (!(window.level[actX][actY-1] instanceof Wall) && 
+					!(window.level[actX][actY-1] instanceof Door) && 
+					!(window.level[actX][actY-1] instanceof Key))
+				neighbors[0] = new Node(actX, actY-1); // up
+			else
+				neighbors[0] = null;
+			
+			// Right-Neighbor valid?
+			if (!(window.level[actX+1][actY] instanceof Wall) && 
+					!(window.level[actX+1][actY] instanceof Door) && 
+					!(window.level[actX+1][actY] instanceof Key))
+				neighbors[1] = new Node(actX+1, actY); // right
+			else
+				neighbors[1] = null;
+			
+			// Down-Neighbor valid?
+			if (!(window.level[actX][actY+1] instanceof Wall) && 
+					!(window.level[actX][actY+1] instanceof Door) && 
+					!(window.level[actX][actY+1] instanceof Key))
+				neighbors[2] = new Node(actX, actY+1); // down
+			else
+				neighbors[2] = null;
+			
+			// Left-Neighbor valid?
+			if (!(window.level[actX-1][actY] instanceof Wall) && 
+					!(window.level[actX-1][actY] instanceof Door) && 
+					!(window.level[actX-1][actY] instanceof Key))
+				neighbors[3] = new Node(actX-1, actY); // left
+			else
+				neighbors[3] = null;
+			
+			// Check all Neighbors
+			for (int k=0; k<4; k++){
+				if (neighbors[k] == null)
+					continue;
+				if (neighbors[k].getXPos() == player.getXPos() && neighbors[k].getYPos() == player.getYPos()){
+					// Current Neighbor is the goal / player
+					playerFound = true;
+				}
+				else {
+					// Calculate costs of neighbor
+					neighbors[k].setCostFromStart(cheapest.getCostFromStart() + 1);
+					int xDiff = player.getXPos() - neighbors[k].getXPos();
+					int yDiff = player.getYPos() - neighbors[k].getYPos();
+					neighbors[k].setCostToGoal((int) Math.sqrt(xDiff*xDiff + yDiff*yDiff));
+					neighbors[k].calculateCosts();
+					
+					boolean skipNode = false;
+					for (Node checkOpen : openList){
+						if (checkOpen.getXPos() == neighbors[k].getXPos() && checkOpen.getYPos() == neighbors[k].getYPos()){
+							// Neighbor already in the openList
+							if(checkOpen.getCalculatedCosts() <= neighbors[k].getCalculatedCosts())
+								skipNode = true;
+						}
+					}
+					for (Node checkClosed : closedList){
+						if (checkClosed.getXPos() == neighbors[k].getXPos() && checkClosed.getYPos() == neighbors[k].getYPos()){
+							// Neighbor already in the closedList
+							if(checkClosed.getCalculatedCosts() <= neighbors[k].getCalculatedCosts())
+								skipNode = true;
+						}
+					}
+					if (!skipNode)
+						openList.add(neighbors[k]);
+				}
+			}// for-neighbors
+			closedList.add(cheapest);
+		}// while
+		
+		// Change this.dir
+		Node nextStep = closedList.get(1);
+		if (nextStep.getXPos() == this.getXPos() && nextStep.getYPos() == this.getYPos()-1)
+			this.dir = 0; // go up
+		else if (nextStep.getXPos() == this.getXPos()+1 && nextStep.getYPos() == this.getYPos())
+			this.dir = 1; // go right
+		else if (nextStep.getXPos() == this.getXPos() && nextStep.getYPos() == this.getYPos()+1)
+			this.dir = 2; // go down
+		else if (nextStep.getXPos() == this.getXPos()-1 && nextStep.getYPos() == this.getYPos())
+			this.dir = 3; // go left
+		else
+			return false;
+		
+		return true;
 	}
 	
 	public int getType(){
---
+++ Hindi/src/pp2016/team19/shared/Node.java
@@ -0,0 +1,39 @@
+package pp2016.team19.shared;
+
+public class Node {
+	private int xPos, yPos;
+	private int costFromStart, costToGoal, calculatedCosts;
+	
+	public Node(int _x, int _y){
+		this.xPos = _x;
+		this.yPos = _y;
+		this.costFromStart = 0;
+		this.costToGoal = 0;
+	}
+	
+	// Getter and Setter
+	public void setCostFromStart(int _cost){
+		this.costFromStart = _cost;
+	}
+	public int getCostFromStart(){
+		return costFromStart;
+	}
+	public void setCostToGoal(int _cost){
+		this.costToGoal = _cost;
+	}
+	public int getCostToGoal(){
+		return costToGoal;
+	}
+	public void calculateCosts(){
+		this.calculatedCosts = this.costFromStart + this.costToGoal;
+	}
+	public int getCalculatedCosts(){
+		return calculatedCosts;
+	}
+	public int getXPos() {
+		return xPos;
+	}
+	public int getYPos() {
+		return yPos;
+	}
+}
