50b05962eb3d18e2c689727c68616664a771324c
Oliver <goetzoliver@web.de>
2016-06-18 14:13:57 +0200

    Message Class: toString method overwritten

--- Hindi/src/pp2016/team19/shared/Message.java
+++ Hindi/src/pp2016/team19/shared/Message.java
@@ -1,14 +1,18 @@
 package pp2016.team19.shared;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public abstract class Message implements Serializable {
 	
+	private final long timestamp;
 	private int type;
 	private int subType;
 	private int clientID;
 	
 	public Message(int type, int subType, int clientID){
+		this.timestamp = System.currentTimeMillis();
 		this.type = type;
 		this.subType = subType;
 		this.clientID = clientID;
@@ -33,6 +37,19 @@ public abstract class Message implements Serializable {
 	}
 	public int getClientID(){
 		return clientID;
+	}
+	
+	/**
+	 * @ author Oliver Götz, 5961343
+	 * 
+	 * This method overwrites the inherited String-Method
+	 */
+	public String toString() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(
+				"yyyy-MM-DD hh:mm:ss");
+		return (dateFormat.format(new Date(this.timestamp)) + ":: ClientID :"
+				+ this.getClientID() + " PlayerID: " 
+				+ " Type: " + this.getType() + " Subtype: " + this.getSubType());
 	}
 	
 }
