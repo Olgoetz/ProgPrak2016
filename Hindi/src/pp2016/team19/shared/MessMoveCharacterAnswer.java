13d2ce2b2d7529ce2f7aa36b6ff94d2f721ec7ff
Oliver <goetzoliver@web.de>
2016-06-18 17:49:07 +0200

    MessMoveCharacter Classes added

---
+++ Hindi/src/pp2016/team19/shared/MessMoveCharacterAnswer.java
@@ -0,0 +1,32 @@
+package pp2016.team19.shared;
+
+import java.io.Serializable;
+
+public class MessMoveCharacterAnswer extends Message implements Serializable {
+
+	/**
+	 * @author Oliver Goetz, 5961343
+	 * Move-Down Request from server.
+	 */
+	private static final long serialVersionUID = 1L;
+	int posX;
+	int posY;
+	boolean confirmed;
+	
+	public MessMoveCharacterAnswer(int posX, int posY, int type, int subType, boolean pConfirmed) {
+		super(type, subType);
+		this.posX = posX;
+		this.posY = posY;
+		this.confirmed = pConfirmed;
+	}
+	
+	public void setConfirmed (boolean pConfirmed) {
+		this.confirmed = pConfirmed;
+	}
+	
+	public boolean isConfirmed() {
+		return confirmed;
+	}
+	
+	
+}
