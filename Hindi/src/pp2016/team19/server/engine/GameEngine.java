221a2cab92e491748488974e310e3e5248c49257
Tobi <Tobi@Tobi-HP.localdomain>
2016-06-20 19:18:41 +0200

    Finetuning, added some Javadocs

--- Hindi/src/pp2016/team19/server/engine/GameEngine.java
+++ Hindi/src/pp2016/team19/server/engine/GameEngine.java
@@ -4,32 +4,37 @@ import java.util.TimerTask;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import pp2016.team19.shared.*;
-
+/**
+ * 
+ * @author Tobias Schrader
+ *
+ */
 public class GameEngine extends TimerTask{
 	LinkedBlockingQueue<Message> messagesFromGame;
 	Game game;
 	Player player;
-	public GameEngine(Game game, Player player,LinkedBlockingQueue<Message> messagesFromGame) {
+	ServerEngine engine;
+	public GameEngine(ServerEngine engine, Game game, Player player,LinkedBlockingQueue<Message> messagesFromGame) {
 		this.game=game;
 		this.player=player;
 		this.messagesFromGame = messagesFromGame;
+		this.engine = engine;
 	}
 	
 	public void run() {
 		for(Monster monster: game.Monsters) {
 			//Move Monster
 		}
-		try { //Player Commands
+//Player Commands
 			Message message = this.messagesFromGame.poll();
 			if (message != null) {
 				this.distributor(message);
 			}
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-
+	/**
+	 * Determines action depending on subtype
+	 * @param message
+	 */
 	public void distributor(Message message) {
 		switch(message.getSubType()) {
 		case Move:
@@ -38,15 +43,18 @@ public class GameEngine extends TimerTask{
 			break;
 		}
 	}
-
+/**
+ * Executes player movement command
+ * @param message
+ */
 	private void playerMove(Message message) {
 		switch(message.direction) {
 		case 0: //MoveUp
 			if (game.gameMap[player.getXPos()][player.getYPos()+1].isFloor()) {
 				player.setPos(player.getXPos(),player.getYPos()+1);
-				engine.MessagesToClient.addElement(MessMoveCharacterAns(player.getXPos(),player.getYPos(),type,subtype,true));
+				engine.messagesToClient.addElement(MessMoveCharacterAns(player.getXPos(),player.getYPos(),type,subtype,true));
 			} else {
-				engine.MessagesToClient.addElement(MessMoveCharacterAns(player.getXPos(),player.getYPos(),type,subtype,false));
+				engine.messagesToClient.addElement(MessMoveCharacterAns(player.getXPos(),player.getYPos(),type,subtype,false));
 			}
 				
 		}
