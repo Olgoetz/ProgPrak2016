c7785eb2211a609b53afb184489c5f91d03a52ab
Taner Bulut <tanerbulut@taners-mbp>
2016-06-23 00:49:51 +0200

    Auskommentierung der Testsimulation bei server und Client MAin

--- Hindi/src/pp2016/team19/client/engine/ClientMain.java
+++ Hindi/src/pp2016/team19/client/engine/ClientMain.java
@@ -16,31 +16,31 @@ public class ClientMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		new Engine();
+		new Engine();
 		
 		/*TESTS FOR SENDING MESSAGES FROM CLIENT TO SERVER 
 		 *AND RECEIVING MESSAGES FROM SERVER TO CLIENT 
 		 **/
-		TestMessage test1 = new TestMessage(0,1,"TestMessage1 from Client to Server");
-		TestMessage test2 = new TestMessage(0,1,"TestMessage2 from Client to Server");
-		TestMessage test3 = new TestMessage(0,1,"TestMessage3 from Client to Server");
-		TestMessage test4 = new TestMessage(0,1,"TestMessage4 from Client to Server");
-		Message message;
-		boolean sent = false;
-		NetworkHandlerC nhc = new NetworkHandlerC();
-		while(true){
-			message = nhc.getMessageFromServer();
-			if(message != null){
-				System.out.println("TestMessage received from Server : "+message.toString());
-			}
-			else if(!sent){
-			nhc.sendMessageToServer(test1);
-			nhc.sendMessageToServer(test2);
-			nhc.sendMessageToServer(test3);
-			nhc.sendMessageToServer(test4);
-			sent = true;
-			}
-		}
+//		TestMessage test1 = new TestMessage(0,1,"TestMessage1 from Client to Server");
+//		TestMessage test2 = new TestMessage(0,1,"TestMessage2 from Client to Server");
+//		TestMessage test3 = new TestMessage(0,1,"TestMessage3 from Client to Server");
+//		TestMessage test4 = new TestMessage(0,1,"TestMessage4 from Client to Server");
+//		Message message;
+//		boolean sent = false;
+//		NetworkHandlerC nhc = new NetworkHandlerC();
+//		while(true){
+//			message = nhc.getMessageFromServer();
+//			if(message != null){
+//				System.out.println("TestMessage received from Server : "+message.toString());
+//			}
+//			else if(!sent){
+//			nhc.sendMessageToServer(test1);
+//			nhc.sendMessageToServer(test2);
+//			nhc.sendMessageToServer(test3);
+//			nhc.sendMessageToServer(test4);
+//			sent = true;
+//			}
+//		}
 		
 		//testEngine.getNetworkHandler();
 		
