3392d15ba9adb3783596fbc3a6b344870524a3d0
Fee <Fee@192.168.178.65>
2016-06-22 19:30:13 +0200

    paar Kommentare, menupanel verbessert

--- Hindi/src/pp2016/team19/client/gui/MenuPanel.java
+++ Hindi/src/pp2016/team19/client/gui/MenuPanel.java
@@ -10,46 +10,41 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 // author: Felizia Langsdorf, Matr_Nr: 6002960
+
 // class for the menu, shown after successful login
+// problems occurred with the paint method, read the text below at the end of this class
 
 public class MenuPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-	private Image menu;
+//	private Image menu;
 	private GameWindow window;
-	
 	public JPanel p;
 	private JButton b1;
 	private JButton b2;
 	
-	//constructor of the menupanel
-	
+	//constructor of the menupanel	
 	public MenuPanel(GameWindow window){
 		
 		this.window = window;
-		try{
-			menu=ImageIO.read(new File("img//menu.png"));
-		} catch (IOException e) {
-			System.err.println("Error while loading one of the images.");
-		}
+//		try{
+//			menu=ImageIO.read(new File("img//menu.png"));
+//		} catch (IOException e) {
+//			System.err.println("Error while loading one of the images.");
+//		}
 		
+		// create panel, set its color and size, etc.
 		p = new JPanel();
-		p.setLayout(null);
 		p.setBackground(Color.LIGHT_GRAY);
-		//p.setSize(400, 300);
-		
+		p.setPreferredSize(new Dimension(400, 500)); 	
 		p.setLayout( new GridLayout( 2, 0 ));
-		//p.setBounds(0, 0, 400, 600);
 		p.setVisible(true);
-		 
+		// create the two buttons
 		b1 = new JButton("Start Game");
 		b2 = new JButton("Logout");
-//		b1.setBounds(0, 0, 200, 100);
-//		b2.setBounds(0, 0, 200, 100);
-
 		// adding the panel to the gamewindow
-		add(p);
+		add(p, BorderLayout.CENTER);
 		// adding the buttons to the panel
 		p.add(b1);
 		p.add(b2);
@@ -57,28 +52,26 @@ public class MenuPanel extends JPanel implements ActionListener {
 		b1.addActionListener(this); 	
 		b2.addActionListener(this);			
 	}
-	
-	
-	// man endet nach klicken der buttons leider in einer sackgasse, man kommt nicht zurück, idee menubar dazu machen kooootz
+
 	
 	// method of actionlistener; adding the actions after clicking on the buttons
 	public void actionPerformed(ActionEvent ae) {
-		if(ae.getSource()==b1){	
+		if(ae.getSource()==b1){			//pressing button start game, then game starts 
 			window.showGameField();
 	}else if (ae.getSource() == b2)	{
-		System.exit(0);      //ordentliches ausloggen kommt noch :) jetzt erst einfach mal anwendung schließen
+		System.exit(0);      // here's going to be a proper method for logging out
 	}	
 }
 
-	
-//logout button verschwindet, nachdem außerhalb des frames klickt, taucht er auf wtf???
 //	public void paint(Graphics g){
 //	//g.setColor(Color.GRAY);
 //	//g.fillRect(0, 300, 300,100);
 //	g.drawImage(menu, this.getWidth()/3, 300, null);	
 //	}
-//}
-
+	
+// !!! logout button disappears if anything is being painted, after clicking somewhere randomly outside the window(JFrame) button shows up again. magic?? 
 }
+
+
 
 
