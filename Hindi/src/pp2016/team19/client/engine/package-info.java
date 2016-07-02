34dfc4f86e5905946e5712d62cba348f4820d3cb
Oliver <goetzoliver@web.de>
2016-07-02 18:33:11 +0200

    package-info für client.engine package

---
+++ Hindi/src/pp2016/team19/client/engine/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Comprises the classes to start the client engine.
+ * <p>
+ * It contains two classes:
+ * <p>
+ * 		1. ClientEngine - contains all information about the client, represents the interface 
+ * 		   between GUI and Communication.
+ * <p>
+ * 		2. ClientMain - starts the client engine, after the ServerMain {@link pp2016.team19.server.engine.ServerMain} 
+ * 		   has been started.
+ * <p>
+ * 
+ */
+package pp2016.team19.client.engine;
