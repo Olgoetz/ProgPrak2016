99904330bb653ee2eef83f09d81ef88b78df8697
Fee <Fee@172.29.240.212>
2016-07-11 18:05:42 +0200

    statusbar

--- Hindi/src/pp2016/team19/client/gui/Statusbar.java
+++ Hindi/src/pp2016/team19/client/gui/Statusbar.java
@@ -34,7 +34,7 @@ public class Statusbar extends JPanel {
 
 private static final long serialVersionUID = 1L;
 	
-	private Image background, key, potion;	
+	private Image key, potion;	
 	private Image floor1, wall1, playerImg; 
 	private Image red, black, beige;
 	
