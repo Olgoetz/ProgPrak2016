785f33a7718077007dacf40755673573f1636669
Fee <Fee@172.29.240.58>
2016-07-12 14:25:51 +0200

    menupanel final update

--- Hindi/src/pp2016/team19/client/gui/MenuPanel.java
+++ Hindi/src/pp2016/team19/client/gui/MenuPanel.java
@@ -3,10 +3,6 @@ package pp2016.team19.client.gui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
