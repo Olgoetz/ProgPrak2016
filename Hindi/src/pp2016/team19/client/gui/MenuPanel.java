7801c23c5c9b8562c1ea350da5cfc1733f6f354b
Fee <Fee@172.29.240.58>
2016-07-12 14:24:52 +0200

    menupanel update

--- Hindi/src/pp2016/team19/client/gui/MenuPanel.java
+++ Hindi/src/pp2016/team19/client/gui/MenuPanel.java
@@ -10,33 +10,31 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
- * <h1> class for the welcome panel shows up after a successful login. <h1>
- * 
+ * <h1>class for the welcome panel shows up after a successful login.<h1>
  * @author Felizia Langsdorf, 6002960
  */
 
 public class MenuPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	//background image
-	private ImageIcon picture = new ImageIcon("img//labyrinth.png");//source:http://brandedinthe80s.com/wp-content/uploads/2014/03/2013-04-10-labyrinth.gif
+	// background image
+	private ImageIcon picture = new ImageIcon("img//labyrinth.png");// source:http://brandedinthe80s.com/wp-content/uploads/2014/03/2013-04-10-labyrinth.gif
 	private JLabel lab;
-	//gamewindow JFrame
+	// gamewindow JFrame
 	private GameWindow window;
-	//two buttons
+	// two buttons
 	private JButton b1;
 	private JButton b2;
-	
 
 	/**
-	 * @author Felizia Langsdorf, Matr_Nr: 6002960
+	 * @author Felizia Langsdorf, 6002960
 	 * @param window
 	 *            gamewindow of the application
 	 */
 	public MenuPanel(GameWindow window) {
 
 		this.window = window;
-		//layout of the panel
+		// layout of the panel
 		this.setLayout(new FlowLayout());
 
 		// add the label (picture) on the panel
@@ -57,25 +55,28 @@ public class MenuPanel extends JPanel implements ActionListener {
 		b2.setOpaque(true);
 		b1.setVisible(true);
 		b2.setVisible(true);
-		//adding the actionlistener to the buttons
+		// adding the actionlistener to the buttons
 		b1.addActionListener(this);
 		b2.addActionListener(this);
-		
+
 	}
 
 	/**
-	 * @author Felizia Langsdorf, 6002960
 	 * method of the interface actionListener: after clicking the buttons the
 	 * game starts or logout is initiated
+	 * 
+	 * @author Felizia Langsdorf, 6002960
+	 * @param ae
+	 *            ActionEvent
+	 * 
 	 */
 
 	public void actionPerformed(ActionEvent ae) {
 		if (ae.getSource() == b1) { // button start game
-			window.initializeGameframe(32*16, 32*16, "Hindi Bones"); //initialize the window for playing
+			window.initializeGameframe(32 * 16, 32 * 16, "Hindi Bones"); // initializes the window for playing the game																			
 			window.getEngine().getThreadPool().execute(window);
-
 		} else if (ae.getSource() == b2) {
-			window.engine.serverSignOutRequest(); //logout
+			window.engine.serverSignOutRequest(); // logout
 		}
 	}
 
